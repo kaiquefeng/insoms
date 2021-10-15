24328a8e01d7ecd61ae1635a85085c794d6848fe
kaiquemaia <kaiquefeng1@gmail.com>
2021-10-15 00:27:25 -0300

    chore: styles application

--- src/services/baseImages.ts
+++ src/services/baseImages.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiImages = axios.create({
-  baseURL: "http://localhost:4000/api/images",
+  baseURL: "http://localhost:3000/api/images",
   headers: { "Content-Type": "application/json", Accept: "application/json" },
 });
 
